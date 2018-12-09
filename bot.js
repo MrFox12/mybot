d367b752e7d21cf22c94d12f22c14024440d8196
MrFox12 <32931308+MrFox12@users.noreply.github.com>
2018-12-09 09:07:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const prefix = "*"
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === `${prefix}claim`) {
+    message.author.setRole("⚜️ALYSINATE⚜️")
   }
 });
 
